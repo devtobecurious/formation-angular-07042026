75a7551cc013f25a1391850e217c706174f40188
Evan BOISSONNOT <evan@boissonnot.fr>
2026-04-09 16:56:45 +0200

    feat: input/ouput signal

--- selfie-a-wookiees/src/app/features/profile/components/display-list-selfies/display-list-selfies.ts
+++ selfie-a-wookiees/src/app/features/profile/components/display-list-selfies/display-list-selfies.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, input, Input, OnChanges, output, SimpleChanges } from '@angular/core';
 import { Selfie } from '../../models/selfie';
 
 @Component({
@@ -7,6 +7,17 @@ import { Selfie } from '../../models/selfie';
   templateUrl: './display-list-selfies.html',
   styleUrl: './display-list-selfies.css',
 })
-export class DisplayListSelfies { // Dummy component / agnostic
-  @Input({ required: true }) selfies: Selfie[] = [];
+export class DisplayListSelfies implements OnChanges {
+  // Dummy component / agnostic
+  //@Input({ required: true }) selfies: Selfie[] = [];
+  selfies = input.required<Selfie[]>();
+  deleteSelfie = output<Selfie>();
+
+  ngOnChanges(changes: SimpleChanges): void {
+    console.info('Changes detected in DisplayListSelfies component:', changes);
+  }
+
+  onDelete(selfie: Selfie): void {
+    this.deleteSelfie.emit(selfie);
+  }
 }
--- selfie-a-wookiees/src/app/features/profile/components/edit-profile/edit-profile.ts
+++ selfie-a-wookiees/src/app/features/profile/components/edit-profile/edit-profile.ts
@@ -2,6 +2,7 @@ import { Component, EventEmitter, inject, Output } from '@angular/core';
 import { DisplaySelectPlanets } from '../display-select-planets/display-select-planets';
 import { DisplayListSelfies } from '../display-list-selfies/display-list-selfies';
 import { GetSelfiesFromWookiee } from '../../services/get-selfies-from-wookiee';
+import { Selfie } from '../../models/selfie';
 
 @Component({
   selector: 'app-edit-profile',
@@ -12,12 +13,16 @@ import { GetSelfiesFromWookiee } from '../../services/get-selfies-from-wookiee';
 export class EditProfile {
   private readonly wookieId = 1;
   private readonly getSelfiesFromWookiee = inject(GetSelfiesFromWookiee);
-  protected readonly dataSelfies = this.getSelfiesFromWookiee.getAll(this.wookieId);
+  protected dataSelfies = this.getSelfiesFromWookiee.getAll(this.wookieId);
   //constructor(private readonly getSelfiesFromWookiee: GetSelfiesFromWookiee) {}
 
   @Output() saveProfile = new EventEmitter<number>();
 
   onSubmit(): void {
     this.saveProfile.emit(this.wookieId);
+  }
+
+  onDeleteSelfie(selfie: Selfie): void {
+    this.dataSelfies = this.dataSelfies.filter(s => s.id !== selfie.id);
   }
 }
