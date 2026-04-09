426c284da1211cc942ecb635909be2fb9cfa4272
Evan BOISSONNOT <evan@boissonnot.fr>
2026-04-09 11:53:10 +0200

    feat: opeartor rxjs

--- selfie-a-wookiees/src/app/features/profile/components/display-select-planets/display-select-planets.ts
+++ selfie-a-wookiees/src/app/features/profile/components/display-select-planets/display-select-planets.ts
@@ -1,7 +1,7 @@
 import { Component, inject, OnDestroy, OnInit } from '@angular/core';
 import { Planet } from '../../models/planet';
 import { GetAllPlanets } from '../../services/get-all-planets';
-import { Subscription } from 'rxjs';
+import { map, of, Subscription, tap } from 'rxjs';
 
 @Component({
   selector: 'app-display-select-planets',
@@ -18,6 +18,34 @@ export class DisplaySelectPlanets implements OnInit, OnDestroy {
   protected readonly planets: Planet[] = []
 
   ngOnInit(): void {
+    const obs$ = of(1, 2, 3).pipe(
+      tap(value => console.log('Before map: ' + value)),
+      map(value => value * 2),
+      tap(value => console.log('Before map 2: ' + value)),
+      map(value => value + ' !'),
+      tap(value => console.log('after map: ' + value)),
+    );
+
+    obs$.subscribe({
+      next: (value) => console.log(value)
+    });
+
+   //const planets$ = this.getAllPlanets.getAll();
+    // const obs$ = planets$.pipe(
+    //   map(planets => {
+    //     const list: { value: string }[] = [];
+
+    //     planets.forEach(planet => {
+    //       list.push({
+    //         value: `${planet.name} -- ${planet.size} km`,
+    //       })
+    //     })
+
+    //     return list;
+    //   })
+    // )
+
+
     this.parentSubscription.add(
       this.getAllPlanets.getAll().subscribe({
         next: (planets) => {
